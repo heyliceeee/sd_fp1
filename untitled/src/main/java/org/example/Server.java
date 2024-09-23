255d0e79f7b4bf2c589679a15fe76c0caaf063a7
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2024-09-23 10:09:13 +0100

    sparkles ex 3.1.2

--- untitled/src/main/java/org/example/Server.java
+++ untitled/src/main/java/org/example/Server.java
@@ -6,18 +6,18 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) throws IOException {
         // Declaração das variáveis do socket e fluxos de I/O
-        ServerSocket serverSocket = null;
+        ServerSocket svSocket = null;
         Socket clientSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
 
         try {
             // Cria um ServerSocket na porta 7 (tradicionalmente usada para o sv)
-            serverSocket = new ServerSocket(7);
+            svSocket = new ServerSocket(7);
             System.out.println("SV à espera de ligações...");
 
             // Aceita a conexão de um cliente (bloqueante, espera até q um cliente se conecte)
-            clientSocket = serverSocket.accept();
+            clientSocket = svSocket.accept();
             System.out.println("Ligação estabelecida com cliente.");
 
             // Prepara o PrintWriter pra enviar dados pra o cliente
@@ -26,11 +26,16 @@ public class Server {
             // Prepara o BufferedReader pra receber dados do cliente
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+            //obter o ip do sv e do client
+            String svIP = svSocket.getInetAddress().getHostAddress(); //ip do sv
+            String clientIP = clientSocket.getInetAddress().getHostAddress(); //ip do client
+
             String inputLine;
-            // Lê as msgs recebidas do cliente e envia de volta a mm msg
+            // Ler as msgs do cliente e devolver a estrutura: IP SERVIDOR:IP CLIENTE:MENSAGEM
             while ((inputLine = in.readLine()) != null) {
-                System.out.println("Mensagem recebida: " + inputLine);
-                out.println(inputLine); // Envia a mm msg de volta ao cliente
+                System.out.println("msg recebida: " + inputLine);
+                String response = svIP + ":" + clientIP + ":" + inputLine;
+                out.println(response); // Enviar de volta a msg formatada
             }
 
         } catch (IOException e) {
@@ -42,7 +47,7 @@ public class Server {
             if (out != null) out.close();
             if (in != null) in.close();
             if (clientSocket != null) clientSocket.close();
-            if (serverSocket != null) serverSocket.close();
+            if (svSocket != null) svSocket.close();
         }
     }
 }
